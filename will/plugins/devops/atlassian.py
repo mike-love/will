d71e56b555ba3ca3a3a99f3cb8a97be76c2cf9a3
Michael Love <mlove@columnit.com>
2017-05-16 09:51:22 -0400

    corrected issues with initial calls

--- will/plugins/devops/atlassian.py
+++ will/plugins/devops/atlassian.py
@@ -16,11 +16,13 @@ class AtlassianPlugin(WillPlugin):
 
     @respond_to("create(?P<project_type>.*)?project(?P<project_name>.*)")
     def will_create_project(self, message, project_type, project_name):
-        user_prof = self.get_hipchat_user(self, message.sender.nick['email'])
+        user_prof = self.get_hipchat_user(message.sender.nick)
 
         if user_prof.get('email'):
             tmp_un = user_prof['email'].split('@')[0]
             if self._is_valid_user(tmp_un):
+                logging.debug('Could not find user %(user)s in %(server)s'
+                              % {'user': tmp_un, 'server': settings.JIRA_SERVER})
                 proj_admin = tmp_un
             else:
                 proj_admin = settings.JIRA_USERNAME
@@ -32,8 +34,8 @@ class AtlassianPlugin(WillPlugin):
                    % (project_name, proj_admin))
 
         try:
-            hc = self.create_hipchat_room(project_name, owner=message.sender.nick['email'])
-            self.reply(message, "created hipchat room %(room)%s" %{room: hc.get('name')})
+            hc = self.create_hipchat_room(project_name, owner=message.sender.nick)
+            self.reply(message, "created hipchat room %(room)%s" %{'room': hc.get('name')})
         except:
             raise
 
@@ -66,7 +68,7 @@ class AtlassianPlugin(WillPlugin):
                                        space_admin=proj_admin, blueprint=True,
                                        context_element=context_element,
                                        blueprint_id="22dd1292-0487-406b-9c89-d342e6d7e8cd")
-            self.reply(message, 'Created atlassian project %(name)' % {'name': project_name})
+            self.reply(message, 'Created atlassian project %(name)s' % {'name': project_name})
         except:
             raise
 
