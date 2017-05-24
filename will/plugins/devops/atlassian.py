617ac67956ebfcce1d82bdbf799cefef59585192
Michael Love <mlove@columnit.com>
2017-05-24 14:48:04 -0400

    leveraged user sending message to add administrative role to hc, and jira

--- will/plugins/devops/atlassian.py
+++ will/plugins/devops/atlassian.py
@@ -10,37 +10,35 @@ import logging
 import random
 import json
 import re
-
+import requests
 
 class AtlassianPlugin(WillPlugin):
 
     @respond_to("create(?P<project_type>.*)?project(?P<project_name>.*)")
     def will_create_project(self, message, project_type, project_name):
-        user_prof = self.get_hipchat_user(message.sender.nick)
-
-        if user_prof.get('email'):
-            tmp_un = user_prof['email'].split('@')[0]
-            if self._is_valid_user(tmp_un):
-                logging.debug('Could not find user %(user)s in %(server)s'
-                              % {'user': tmp_un, 'server': settings.JIRA_SERVER})
-
-                proj_admin = tmp_un
-            else:
-                proj_admin = settings.JIRA_USERNAME
-        else:
-            proj_admin = settings.JIRA_USERNAME
-
+        user_email = self.get_hipchat_user('@%s' % message.sender.nick).get('email')
 
         self.reply(message, "ok creating project %s with %s as project lead"
-                   % (project_name, proj_admin))
+                   % (project_name, user_email))
 
         try:
-            hc = self.create_hipchat_room(project_name, owner='dummy@example.com')
+            hc = self.create_hipchat_room(project_name, owner=user_email)
             self.reply(message, "created hipchat room %(room)s" %{'room': hc.get('name')})
         except:
             raise
 
         try:
+            try:
+                ur = self.get_jira_user(user_email.split('@')[0])
+
+                proj_admin = user_email.split('@')[0]
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 404:
+                    proj_admin = settings.JIRA_USERNAME
+
+                else:
+                    raise
+            logging.debug('Creating JIRA project %s with %s as admin/lead' % (project_name, proj_admin))
             j_resp = self.create_jira_project(proj_name=project_name, proj_admin=proj_admin,
                                               proj_template_key="com.pyxis.greenhopper.jira:gh-scrum-template")
 
@@ -54,9 +52,17 @@ class AtlassianPlugin(WillPlugin):
             # reuse the jira key if it's not already assigned in confluence
             if self.space_key_exists(j_resp['key']):
                 space_key = None
-
             else:
                 space_key = j_resp['key']
+
+            try:
+                ur = self.get_confluence_user(user_email.split('@')[0])
+                proj_admin = user_email.split('@')[0]
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 404:
+                    proj_admin = settings.CONFLUENCE_USERNAME
+                else:
+                    raise
 
             context_element = {"jira-server": "6028ff00-2ed7-3998-8913-344d65267cba",
                         "jira-project": j_resp['id'], "name": project_name,
@@ -66,21 +72,13 @@ class AtlassianPlugin(WillPlugin):
                         "project-key": j_resp['key'], "project-name": project_name,
                         "ContentPageTitle": project_name}
 
+            logging.debug('Creating Confluence space %s with %s as admin/lead' % (project_name, proj_admin))
             c_resp = self.create_space(project_name, space_key=space_key,
                                        description="", space_admin=proj_admin,
                                        blueprint=True, context_element=context_element,
                                        blueprint_id="22dd1292-0487-406b-9c89-d342e6d7e8cd")
+
             self.reply(message, 'Created atlassian project %(name)s' % {'name': project_name})
         except:
             raise
-
-
-
-    def _is_valid_user(self, userid):
-        """checks if the user is a valid user"""
-        if(self.get_jira_user(userid).get('email')):
-            return True
-        else:
-            return False
-
 
