433e710f489de0e6b89d50980eff874a8e51e99f
Michael Love <mlove@columnit.com>
2017-06-14 08:50:51 -0400

    updated test scripts for new BasicRESTClient

--- will/tests/test_RESTClient.py
+++ will/tests/test_RESTClient.py
@@ -1,7 +1,7 @@
 from mock import MagicMock, patch
 import unittest
 
-from will.utils import RESTClient, _BasicRESTClient
+from will.utils import RESTClient, BasicRESTClient
 import requests
 import json
 import pep8
@@ -15,6 +15,8 @@ class TestRESTClientPEP8(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          'Found PEP8 complance issues/warnings.')
 """
+"""
+# TODO: Remove Test with class
 class TestRESTClientFactory(unittest.TestCase):
     def setUp(self):
         pass
@@ -22,17 +24,17 @@ class TestRESTClientFactory(unittest.TestCase):
         self.assertIsInstance(RESTClient.client('basic','test.com','mlove','123'),_BasicRESTClient)
         self.assertIsInstance(RESTClient.client('bAsiC','test.com','mlove','123'),_BasicRESTClient)
         self.assertIsInstance(RESTClient.client('BASIC','test.com','mlove','123'),_BasicRESTClient)
-
+"""
 class Test_BasicRESTClient(unittest.TestCase):
     def setUp(self):
-        self.client = RESTClient.client('basic','test.com', 'mlove', '123')
+        self.client = BasicRESTClient('test.com', 'mlove', '123')
 
-    def test_Base_URL(self):
-        self.assertTrue(self.client.base)
     def test__uri_join(self):
         self.assertEqual(self.client._uri_join('/api/v2'),'http://test.com/api/v2')
+
     def test_SessionConstruction(self):
         self.assertIsInstance(self.client._sess, requests.Session)
+
     def test_SessionParams(self):
         self.assertEqual(self.client.base, 'test.com')
         self.assertEqual(self.client._sess.auth.username, 'mlove')
@@ -40,24 +42,42 @@ class Test_BasicRESTClient(unittest.TestCase):
         self.assertEqual(self.client._sess.auth.password, '123')
 
     @patch('requests.Session.request')
-    def test_request_params(self, mock_request):
+    def test_get(self, mock_request):
         r = self.client.request('get','/api/v2',
                                 data=json.dumps({'key':'value'}))
         mock_request.assert_called_with(method='get',
                                         url='http://test.com/api/v2',
-                                        data=json.dumps({'key':'value'}))
+                                        data=json.dumps({'key':'value'}),
+                                        params={})
 
     @patch('requests.Session.request')
-    def test_request_cb(self, mock_request):
+    def test_get_cb(self, mock_request):
         def jsonresp():
             return json.loads(json.dumps({'key':'value'}))
+
         mock_request.return_value = MagicMock(spec='requests.Response', text={'key':'value'},
-                status_code=200, json=jsonresp)
+                                              status_code=200, json=jsonresp)
 
         r = self.client.request('get','/api/v2',raise_for_status=False,
                                 cb=self.client.strip_data,
                                 data=json.dumps({'key':'value'}))
+
         self.assertEqual(r, {'key':'value'})
+
+
+    @patch('requests.Session.request')
+    def test_post(self, mock_request):
+        mock_request.return_value = MagicMock(spec=requests.Response, status_code = 200,
+                                              response=json.dumps({'value':'key'}))
+
+        r = self.client.request('post', '/api/v2',
+                                data=json.dumps({'key':'value'}))
+        mock_request.assert_called_with(method='post',
+                                        url='http://test.com/api/v2',
+                                        data=json.dumps({'key':'value'}),
+                                        params={})
+        self.assertIsInstance(r, requests.Response)
+        self.assertEqual(r, mock_request.return_value)
 
 
     @patch('requests.Session.request')
@@ -66,10 +86,12 @@ class Test_BasicRESTClient(unittest.TestCase):
                                               response=json.dumps({'value':'key'}))
 
         r = self.client.request('post', '/api/v2',
-                                data=json.dumps({'key':'value'}))
+                                params={'key':'value'})
+
         mock_request.assert_called_with(method='post',
                                         url='http://test.com/api/v2',
-                                        data=json.dumps({'key':'value'}))
+                                        params={'key':'value'})
+
         self.assertIsInstance(r, requests.Response)
         self.assertEqual(r, mock_request.return_value)
 
